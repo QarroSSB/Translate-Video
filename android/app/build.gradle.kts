1a3ef6c153cd6cd4e8228282be88b1eb78caa95f
QarroSSB <qarrossb@gmail.com>
2026-09-10 02:51:00 +0300

    Bump YouTube player fix to v0.4.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.qarro.livetranslator"
         minSdk = 29
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.4.1"
+        versionCode = 6
+        versionName = "0.4.2"
     }
 
     buildFeatures {
