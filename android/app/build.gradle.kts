2bc69ae2e322a568895c2502179fcf1b436106cf
QarroSSB <qarrossb@gmail.com>
2026-09-10 04:01:42 +0300

    Bump Phone-Only migration build to v0.4.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.qarro.livetranslator"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.4.2"
+        versionCode = 7
+        versionName = "0.4.3"
     }
 
     buildFeatures {
