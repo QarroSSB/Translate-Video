c0660b13c6f2d61e8a3a556fa7263aae471cd50c
QarroSSB <qarrossb@gmail.com>
2026-09-10 04:23:04 +0300

    Compile v0.5 against Android API 36

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.qarro.livetranslator"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.qarro.livetranslator"
